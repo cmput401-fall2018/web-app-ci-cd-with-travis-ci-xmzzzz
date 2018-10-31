498835658b026c15872a4c46ea18966e3835bc53
Simone Zhang <906835717@qq.com>
2018-10-31 10:58:35 -0600

    Add files via upload

--- test_service.py
+++ test_service.py
@@ -9,15 +9,15 @@ class Test_service(TestCase):
     
     
     def test_bad_random(self):
-        file_content_mock = """10\n12\n14\n16\n18\n20\n30"""
+        file_content_mock1 = """10\n12\n14\n16\n18\n20\n30"""
         #fake_file_path = 'file/path/mock'
-        newfile = mock_open(read_data=file_content_mock)
+        newfile = mock_open(read_data=file_content_mock1)
         with patch('service.open',newfile,create=True) :
-            number = Service.bad_random()
+            number1 = Service.bad_random()
     
-        expected = len(file_content_mock.split('\n'))
-        assert 0 <= number < expected
-        
+        expected = len(file_content_mock1.split('\n'))
+        assert 0 <= number1 < expected
+
         
         
     def test_divide(self):
@@ -26,6 +26,15 @@ class Test_service(TestCase):
         r_val = s.divide(5)
         assert r_val == 2
         
+        s.bad_random = mock.Mock(return_value=1)
+        #r_val2 = s.divide(0)
+        self.assertRaises(ZeroDivisionError,s.divide,0)
+        
+        '''s.bad_random = mock.Mock(return_value=None)
+        r_val3 = s.divide(10)
+        assert (TypeError,services.divide(0))'''
+
+        
     
     def test_abs_plus(self):
         s = Service()
@@ -33,16 +42,19 @@ class Test_service(TestCase):
         r_val = s.abs_plus(-100)
         assert r_val == 101
         
+        r_val1 = s.abs_plus(1e-9)
+        assert r_val1 == 1e-9+1
+        
         
     
     def test_complicated_function(self):
         s = Service()
         s.bad_random = mock.Mock(return_value=10)
-        s.divide = mock.Mock(return_value=2)
-        r_val = s.complicated_function(5)[0]
-        mod_val = s.complicated_function(5)[1]
-        self.assertTrue(r_val == 2)
-        assert mod_val == 0
+        #s.divide = mock.Mock(return_value=2)
+        r_val = s.complicated_function(5)
+        assert r_val == (2,0)
+        
+        self.assertRaises(ZeroDivisionError,s.complicated_function,0)
         
     
         
