826d21b17c058830ee69ad33faf05b61cb7ddf75
Simone Zhang <xinmeng1@ualberta.ca>
2018-10-31 09:53:07 -0600

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-#from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 def test_home ():
     #binary = FirefoxBinary('/usr/bin/firefox')
     #driver = webdriver.Firefox(firefox_binary=binary)
-    driver = webdriver.Chrome()
+    driver = webdriver.Firefox()
     driver.get("http://162.246.157.132:8000")
     name = driver.find_element_by_id("name")
     about = driver.find_element_by_id("about")
